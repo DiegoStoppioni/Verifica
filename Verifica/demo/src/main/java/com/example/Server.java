7768b038e0a67d96dc0d82f8f4738ffae535af78
DiegoStoppioni <stoppioni.diego@itismeucci.com>
2025-11-11 13:53:05 +0100

    Add files via upload

---
+++ Verifica/demo/src/main/java/com/example/Server.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(3000);
+        do {
+            Socket s = ss.accept();
+            MyThread m = new MyThread(s);
+            m.start();
+        } while (true);
+    }
+}
